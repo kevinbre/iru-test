9616d1e7214519df556f8c0b07b8cee3d5b1a27b
kevinbre <b.kevin@hotmail.es>
2024-08-27 17:35:28 -0300

    release: added logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
     return (
-        <div className="w-screen min-h-[100dvh] bg-neutral-950 text-gray-300 flex flex-col justify-center items-center p-6">
-            Iru Arostegui
+        <div className="w-screen min-h-[100dvh] bg-[#111111] text-gray-300 flex flex-col justify-center items-center p-6">
+            <img alt="Iru" src="/iru.png" />
+
             <div className="flex gap-10">
                 <a className="hover:font-bold hover:scale-105 transition-all" href="mailto:iru@hotmail.com">
                     Contacto
